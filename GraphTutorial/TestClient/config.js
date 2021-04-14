240ccf7095753ad2c2683ae89be497d037d0d05a
Windom Wu <drw_08@hotmail.com>
2021-04-14 06:17:22 +0000

    update docker file

--- GraphTutorial/TestClient/config.js
+++ GraphTutorial/TestClient/config.js
@@ -1,14 +1,15 @@
+const config = require('dotenv').config;
 // https://docs.microsoft.com/en-us/graph/tutorials/azure-functions?tutorial-step=3
 const msalConfig = {
     auth: {
-      clientId: 'YOUR_TEST_APP_APP_ID_HERE',
-      authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID_HERE'
+      clientId: provess.env.YOUR_TEST_APP_APP_ID_HERE,
+      authority: "https://login.microsoftonline.com/" + provess.env.YOUR_TENANT_ID_HERE
     }
   };
   
 const msalRequest = {
   // Scope of the Azure Function
   scopes: [
-    'YOUR_AZURE_FUNCTION_APP_ID_HERE/.default'
+    provess.env.YOUR_AZURE_FUNCTION_APP_ID_HERE+'/.default'
   ]
 }
